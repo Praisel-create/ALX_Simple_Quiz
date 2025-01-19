6023a3e74737885d12044256e04a16c75fae38fc
Praisel <praiseozoko3@gmail.com>
2025-01-19 22:14:41 +0100

    Completed Quiz work

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+    let correctAnswer = 4;
+
+    let userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+
+    let feedback = document.getElementById('feedback')
+
+    if (userAnswer == correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } 
+    else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
